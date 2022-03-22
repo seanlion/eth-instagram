2873386a20f5568f7bfc88c83b8a6593ff6ec0e6
seanlion <skyhs3507@likelion.org>
2022-03-23 01:13:41 +0900

    Update Readme.md

--- src/components/App.js
+++ src/components/App.js
@@ -10,6 +10,7 @@ class App extends Component {
 
   async componentWillMount() {
     await this.loadWeb3()
+    await this.loadBlockchainData()
   }
 
   async loadWeb3() {
@@ -25,21 +26,41 @@ class App extends Component {
     }
   }
 
+  async loadBlockchainData() {
+    const web3 = window.web3
+    // Load account
+    const accounts = await web3.eth.getAccounts()
+    console.log("accounts: ", accounts)
+    const networkId = await web3.eth.net.getId()
+    const networkData = Decentragram.networks[networkId]
+    if(networkData){
+      const decentragram = new web3.eth.Contract(Decentragram.abi, networkData.address)
+      this.setState({ decentragram: decentragram })
+    }
+    else{
+      window.alert('Decentragram contract not deployed to detected network.')
+    }
+    this.setState({ account : accounts[0]})
+  }
+
   constructor(props) {
     super(props)
     this.state = {
       account: '',
+      decentragram: null,
+      images: [],
+      loading: true
     }
   }
 
   render() {
     return (
+       /* custom account prop 정의  */
       <div>
         <Navbar account={this.state.account} />
         { this.state.loading
           ? <div id="loader" className="text-center mt-5"><p>Loading...</p></div>
           : <Main
-            // Code...
             />
           }
       </div>
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,6 +5,7 @@ import photo from '../photo.png'
 class Navbar extends Component {
 
   render() {
+    // App.js NavBar에 정의해놓은 props(account)를 사용함.
     return (
       <nav className="navbar navbar-dark fixed-top bg-dark flex-md-nowrap p-0 shadow">
         <a
@@ -17,15 +18,16 @@ class Navbar extends Component {
           Decentragram
         </a>
         <ul className="navbar-nav px-3">
-          <li className="nav-item text-nowrap d-none d-sm-none d-sm-block">
+          <li className="nav-item text-nowrap d-sm-block">
             <small className="text-secondary">
-              <small id="account">{'0x0'}</small>
+              <small id="account">{this.props.account}</small>
             </small>
             { this.props.account
               ? <img
                 className='ml-2'
                 width='30'
                 height='30'
+                alt = "identicon"
                 src={`data:image/png;base64,${new Identicon(this.props.account, 30).toString()}`}
               />
               : <span></span>
